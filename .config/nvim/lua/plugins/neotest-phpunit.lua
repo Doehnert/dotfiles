4aabd9ccc62b272de5a9f2b3171b174bb411f0aa
Elton Doehnert <eltonfd@gmail.com>
2025-05-14 09:24:23 +1000

    Add new test plugin for neovim

--- .config/nvim/lua/plugins/neotest-phpunit.lua
+++ .config/nvim/lua/plugins/neotest-phpunit.lua
@@ -1,40 +1,15 @@
 return {
-  'nvim-neotest/neotest',
+  'vim-test/vim-test',
   dependencies = {
-    'olimorris/neotest-phpunit',
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
+    'tpope/vim-dispatch', -- optional, for async test running
   },
   config = function()
-    local neotest = require 'neotest'
-
-    neotest.setup {
-      adapters = {
-        require 'neotest-phpunit' {
-          phpunit_cmd = function()
-            -- Runs tests via Laravel Sail
-            return { './vendor/bin/sail', 'phpunit', '--testdox' }
-          end,
-        },
-      },
-    }
-
+    vim.g['test#php#phpunit#executable'] = './vendor/bin/sail phpunit'
     -- Keymaps
     local map = vim.keymap.set
-    map('n', '<leader>tn', function()
-      neotest.run.run()
-    end, { desc = '[T]est [N]earest' })
-    map('n', '<leader>tf', function()
-      neotest.run.run(vim.fn.expand '%')
-    end, { desc = '[T]est [F]ile' })
-    map('n', '<leader>tl', function()
-      neotest.run.run_last()
-    end, { desc = '[T]est [L]ast' })
-    map('n', '<leader>to', function()
-      neotest.output.open { enter = true }
-    end, { desc = '[T]est [O]utput' })
-    map('n', '<leader>ts', function()
-      neotest.summary.toggle()
-    end, { desc = '[T]est [S]ummary' })
+    map('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest' })
+    map('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' })
+    map('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' })
+    map('n', '<leader>tv', ':TestVisit<CR>', { desc = '[T]est [V]isit' })
   end,
 }
