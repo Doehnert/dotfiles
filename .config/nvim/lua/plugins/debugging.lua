029805e7a7d29f44e6f8efc17a47d38980a25343
Elton Doehnert <eltonfd@gmail.com>
2025-05-19 20:48:42 +1000

    Update neovim config

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -4,7 +4,10 @@ return {
     'rcarriga/nvim-dap-ui',
   },
   config = function()
-    local dap, dapui = require 'dap', require 'dapui'
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    dapui.setup() -- Critical setup
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
@@ -12,32 +15,48 @@ return {
     dap.listeners.before.launch.dapui_config = function()
       dapui.open()
     end
-    dap.listeners.before.event_terminated.dapui_config = function()
+    dap.listeners.after.event_terminated.dapui_config = function()
       dapui.close()
     end
-    dap.listeners.before.event_exited.dapui_config = function()
+    dap.listeners.after.event_exited.dapui_config = function()
       dapui.close()
     end
 
     dap.adapters.php = {
       type = 'executable',
       command = 'node',
-      args = { '/path/to/vscode-php-debug/out/phpDebug.js' },
+      args = {
+        os.getenv 'HOME' .. '/.local/share/nvim/dap/php-debug/out/phpDebug.js',
+      },
     }
 
-    dap.configurations.php = {
-      {
+    dap.configurations.php = dap.configurations.php or {}
+
+    local function has_config(name)
+      for _, config in ipairs(dap.configurations.php) do
+        if config.name == name then
+          return true
+        end
+      end
+      return false
+    end
+
+    if not has_config 'Listen for Xdebug' then
+      table.insert(dap.configurations.php, {
         type = 'php',
         request = 'launch',
         name = 'Listen for Xdebug',
         port = 9003,
         pathMappings = {
-          ['/var/www/html'] = '${workspaceFolder}', -- Adjust if your container uses a different path
+          ['/var/www/html'] = '${workspaceFolder}',
         },
-      },
-    }
+      })
+    end
 
     vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
     vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+    vim.keymap.set('n', '<Leader>dq', function()
+      require('dapui').close()
+    end, { desc = 'DAP UI: Close' })
   end,
 }
